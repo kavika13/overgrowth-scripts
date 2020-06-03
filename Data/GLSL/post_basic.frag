5051174cf50818b002d4a635d9ad3f69e59c7d9a
Merlyn Morgan-Graham <kavika@gmail.com>
2020-06-02 21:28:14 -0700

    a25

--- Data/GLSL/post_basic.frag
+++ Data/GLSL/post_basic.frag
@@ -5,12 +5,18 @@ uniform sampler2DRect tex2;
 
 void main()
 {	
-	vec3 color_map = texture2DRect( tex, gl_TexCoord[0].st ).rgb;
-	/*float depth = texture2DRect( tex2, gl_TexCoord[0].st ).r;
+	vec3 color;
 	
-	float near = 0.1;
-	float far = 1000.0;
-	float distance = (near) / (far - depth * (far - near));*/
+	vec3 color_map = texture2DRect( tex, gl_TexCoord[0].st ).rgb;
+	float depth = texture2DRect( tex2, gl_TexCoord[0].st ).r;
+	
+	vec3 accum = texture2DRect( tex, gl_TexCoord[0].st ).rgb +
+				 texture2DRect( tex, gl_TexCoord[0].st + vec2(1,0) ).rgb * -0.25 +
+				 texture2DRect( tex, gl_TexCoord[0].st + vec2(-1,0) ).rgb * -0.25 +
+				 texture2DRect( tex, gl_TexCoord[0].st + vec2(0,1) ).rgb * -0.25 +
+				 texture2DRect( tex, gl_TexCoord[0].st + vec2(0,-1) ).rgb * -0.25;
 
-	gl_FragColor = vec4(color_map,1.0);
+	accum *= 100.0;
+
+	gl_FragColor = vec4(accum,1.0);
 }
--- Data/Scripts/timer_level_script.js
+++ Data/Scripts/timer_level_script.js
@@ -2,30 +2,66 @@ var timer = {};
 
  (function() {
    var time = 0;
+   var time2 = 0;
+   var time3 = 0;
+   var interval;
+   var interval2;
+   var interval3;
+   /*Graphics.setPostEffects(false);*/
  
    function decrTimer() {
-     time--;
      Text.setValue("timerText",String(time));
-     if (time <= 0) {
+     time--;
+     if (time < 0) {
+       time = 0;
        timer.stop();
+     }
+   }
+   
+   function decrTimer2() {
+     time2--;
+     Camera.setFOV(time2+90);
+     if (time2 <= 0) {
+       time2 = 0;
+       Camera.setFOV(90);
+       Text.setValue("raceText","...");
+       clearInterval(interval2);
+     }
+   }
+
+   function decrTimer3() {
+     time3--;
+     if (time3 <= 0) {
+       time3 = 0;
+       Text.setValue("raceText","...");
+       clearInterval(interval3);
      }
    }
  
    timer.start = function(limit) {
-     time = limit;
+     time = limit/2;
      interval = setInterval(decrTimer,1000);
      Text.createText("timerText","TopCenter");
+     Text.createText("raceText","BottomCenter");
      Text.setValue("timerText",String(time));
+     Text.setValue("raceText","Race started!");
+     time3 = 3;
+     interval3 = setInterval(decrTimer3,1000);
+     /*Graphics.setPostEffects(true);*/
    };
 
    timer.addTime = function(amount) {
-     time += amount;
+     time += amount/2;
+     Text.setValue("raceText","Checkpoint!");
+     time2 = 20;
+     interval2 = setInterval(decrTimer2,50);
    };
 
    timer.stop = function() {
+     Graphics.setPostEffects(false);
      clearInterval(interval);
-     if (time > 0) Text.setValue("timerText","You win!");
-     else Text.setValue("timerText","You lose.");
+     if (time > 0) Text.setValue("raceText","You win!");
+     else Text.setValue("raceText","You lose.");
    };
  
  })();
